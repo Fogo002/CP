74c15b48f74257f67b17906965c7ae22020dbcdb
Fogo002 <andreferreiragemeo@gmail.com>
2022-10-15 21:22:07 +0100

    inicializa

--- Guiao1/include/utils.h
+++ Guiao1/include/utils.h
@@ -1,6 +1,7 @@
 #ifndef ___UTILS___
 #define ___UTILS___
+#include <math.h>
 
-
+float euclidean_distance(float ponto1_x,float ponto1_y,float ponto2_x,float ponto2_y);
 
 #endif //___USER_H___
--- Guiao1/src/k_means.c
+++ Guiao1/src/k_means.c
@@ -1,5 +1,37 @@
-#include<stdio.h>
+#include "../include/k_means.h"
+
+#define N 10000000 
+#define K 4
+
+
+void inicializa(float** pontos_x,float** pontos_y,float** cluster_x,float** cluster_y) {
+
+    *pontos_x = malloc(N*sizeof(float));
+    *pontos_y = malloc(N*sizeof(float));
+    *cluster_x = malloc(K*sizeof(float));
+    *cluster_y = malloc(K*sizeof(float));
+
+    srand(10);
+    for(int i = 0; i < N; i++) {
+        (*pontos_x)[i] = (float) rand() / RAND_MAX;
+        (*pontos_y)[i] = (float) rand() / RAND_MAX;
+    }
+    for(int i = 0; i < K; i++) {
+        (*cluster_x)[i] = (*pontos_x)[i];
+        (*cluster_y)[i] = (*pontos_y)[i];
+    }
+}
 
 int main(){
+    float* pontos_x;
+    float* pontos_y;
+    float* cluster_x;
+    float* cluster_y;
+    inicializa(&pontos_x,&pontos_y,&cluster_x,&cluster_y);
+    
+    for(int i = 0; i < N; i++) {
+        //printf("Ponto %d :%f , %f \n",i,pontos_x[i],pontos_y[i]); 
+    }
+
     return 0;
 }
