5f2306dc8437055195039aca9ee9d34dba351b4e
Fogo002 <andreferreiragemeo@gmail.com>
2022-10-16 17:54:44 +0100

    alguns ajustes - info debbug

--- Guiao1/src/k_means.c
+++ Guiao1/src/k_means.c
@@ -1,6 +1,6 @@
 #include "../include/k_means.h"
 
-#define N 1000
+#define N 10000000
 #define K 4
 
 
@@ -13,14 +13,18 @@ void cluster_distrib(float** pontos_x,float** pontos_y,float** cluster_x,float**
         (*cluster_size)[i] = 0.0;
         
     }
-
+    //printf("Distribuição pelos clusters\n");
 
     for(int i = 0; i < N; i++) {
         cluster_atual=0;
         min_dist = euclidean_distance((*cluster_x)[0],(*cluster_y)[0],(*pontos_x)[i],(*pontos_y)[i]);
-        for(int j = 1; j < K ; j++){
+        //printf("Ponto x: %f , y:%f\n",(*pontos_x)[i],(*pontos_y)[i]);
+        //printf("Tentar cluster 0. Dist: %f\n",min_dist);
 
+        for(int j = 1; j < K ; j++){
+            
             tmp = euclidean_distance((*cluster_x)[j],(*cluster_y)[j],(*pontos_x)[i],(*pontos_y)[i]);
+            //printf("Tentar cluster %d. Dist: %f\n",j,tmp);
             if(tmp < min_dist){
                 min_dist = tmp;
                 cluster_atual = j;
@@ -28,8 +32,15 @@ void cluster_distrib(float** pontos_x,float** pontos_y,float** cluster_x,float**
         }
         (*cluster_atribution)[i] = cluster_atual;
         (*cluster_size)[cluster_atual]++;
+        //printf("Cluster: %d\n\n\n",(*cluster_atribution)[i]);
+    }
+    //printf("Distribuição atual\n");
+    for(int i = 0; i < N; i++) {
+        //printf("Ponto x: %f , y: %f --->%d\n",(*pontos_x)[i],(*pontos_y)[i],(*cluster_atribution)[i]);
 
     }
+
+
 }
 
 
@@ -86,29 +97,27 @@ void calculate_centroid(float** pontos_x,float** pontos_y,float** cluster_x,floa
     for(int i = 0; i < K; i++){
         (*centroids_x)[i] = (*centroids_x)[i]/(*cluster_size)[i];
         (*centroids_y)[i] = (*centroids_y)[i]/(*cluster_size)[i];
+        //printf("CLuster: %d\nCentroid calculado x: %f, y: %f\n",i,(*centroids_x)[i],(*centroids_y)[i]);
     }
 }
 
 int new_clusters(float** pontos_x,float** pontos_y,float** cluster_x,float** cluster_y,int** cluster_atribution,
                   float** centroids_x,float** centroids_y,int** cluster_size){
     int boolean = 1;
-    float x,y,tmp,new_x,new_y,min_dist=1000;
+    float x,y,tmp,new_x,new_y,min_dist=2;
     for(int i = 0; i < K; i++) {
         x = (*centroids_x)[i];
         y = (*centroids_y)[i];
 
-        min_dist=1000;
-        for(int j = 0; j<N;j++){
-            if((*cluster_atribution)[j] == i){
-                //printf("Alterar cluster?\n");
-                tmp = euclidean_distance(x,y,(*pontos_x)[j],(*pontos_y)[j]);
-                //printf("Cluster: %d\nPonto x: %f , y:%f\nDist: atual->%f menor-> %f\n\n",i,(*pontos_x)[j],(*pontos_y)[j],tmp,min_dist);
-                if(tmp < min_dist){
-                    //printf("Dist menor encontrado\n");
-                    min_dist = tmp;
-                    new_x = (*pontos_x)[j];
-                    new_y = (*pontos_y)[j];
-                }
+        min_dist=2;
+        for(int j = 0; j<N;j++){ 
+            tmp = euclidean_distance(x,y,(*pontos_x)[j],(*pontos_y)[j]);
+            //printf("Cluster: %d\nPonto x: %f , y:%f\nDist: atual->%f menor-> %f\n\n",i,(*pontos_x)[j],(*pontos_y)[j],tmp,min_dist);
+            if(tmp < min_dist){
+                min_dist = tmp;
+                new_x = (*pontos_x)[j];
+                new_y = (*pontos_y)[j];
+                //printf("Dist menor atualizado %f\n\n",min_dist);
             }
 
         }
@@ -140,6 +149,9 @@ int main(){
     float* centroids_x;
     float* centroids_y;
     inicializa(&pontos_x,&pontos_y,&cluster_x,&cluster_y,&cluster_atribution,&centroids_x,&centroids_y,&cluster_size);
+    for(int i = 0; i < N; i++) {
+        //printf("Ponto %f %f\n",pontos_x[i],pontos_y[i]); 
+    }  
 /*
     printf("Iteração nº 1\n");
 
--- Guiao1/src/utils.c
+++ Guiao1/src/utils.c
@@ -3,5 +3,5 @@
 #include "../include/utils.h"
 
 float euclidean_distance(float ponto1_x,float ponto1_y,float ponto2_x,float ponto2_y){
-    return  (float) sqrt(pow(ponto2_x - ponto1_x, 2)+ pow(ponto2_y - ponto1_y, 2) * 1.0);
+    return  (float) sqrt(pow(ponto2_x - ponto1_x, 2)+ pow(ponto2_y - ponto1_y, 2));
 }
