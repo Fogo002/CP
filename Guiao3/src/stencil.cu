e1b2761006c5bd5ef7df11838dd73130767c8131
Campos <brunitocampos@gmail.com>
2022-12-20 18:34:12 +0000

    ignore

--- Guiao3/src/stencil.cu
+++ Guiao3/src/stencil.cu
@@ -1,12 +1,10 @@
 #include "stencil.h"
 
+using namespace std;
+
 #define NUM_BLOCKS 1024
 #define NUM_THREADS_PER_BLOCK 1024
 #define SIZE NUM_BLOCKS*NUM_THREADS_PER_BLOCK
-
-using namespace std;
-
-
 #define N NUM_BLOCKS*NUM_THREADS_PER_BLOCK
 #define K 4
 
@@ -15,57 +13,48 @@ int* cluster_atribution;  //Array de tamanho N   indexa cada ponto para o seu cl
 int* cluster_size;        //Array de tamnho  K   tamanho de cada cluster
 float* centroids;         //Array de tamnho  K*2*2 com [x,y,old_x,old_y,....] em sequencia de cada centroid atual e do centroid antigo
                           //Sendo que o centroid i = 0 diz respeito ao cluster 0, i = 4*1 diz respeito ao cluster 1, etc 
-
 float* d_pontos;            
 int* d_cluster_atribution; 
 int* d_cluster_size;       
 float* d_centroids; 
-
 int iteracoes = -1;
 
-
 //Distribui os pontos pelos diferentes clusters tendo em conta qual o centroids mais próximo
-__global__  void cluster_distrib(int*q_cluster_size,float*q_centroids,float*q_pontos,int* q_cluster_atribution) {
-        
-        int id = blockIdx.x * blockDim.x + threadIdx.x;
-        int lid = threadIdx.x;
+__global__  void cluster_distrib(int*q_cluster_size,float*q_centroids,float*q_pontos,int* q_cluster_atribution) {        
+    int id = blockIdx.x * blockDim.x + threadIdx.x;
+    int lid = threadIdx.x;
 
-        //__shared__ float temp[NUM_THREADS_PER_BLOCK*2];
-        //temp[lid*2] = pontos[id];
-        //temp[lid*2+1] = pontos[id+1];
+    //__shared__ float temp[NUM_THREADS_PER_BLOCK*2];
+    //temp[lid*2] = pontos[id];
+    //temp[lid*2+1] = pontos[id+1];
 
+    int cluster_atual,clust,k_size=K*4;
+    float min_dist,tmp;
+    float x1,x2,y1,y2;
 
-        int cluster_atual,clust,k_size=K*4;
-        float min_dist,tmp;
-        float x1,x2,y1,y2;
-    
+    cluster_atual=0;
+    min_dist = 2;
+    clust=0;
+    x2 = q_pontos[id*2];
+    y2 = q_pontos[id*2+1];
 
-        cluster_atual=0;
-        min_dist = 2;
-        clust=0;
-        x2 = q_pontos[id*2];
-        y2 = q_pontos[id*2+1];
-
-        for(int j = 0; j < k_size ; j+=4){
-                x1 = q_centroids[j];
-                y1 = q_centroids[j+1];
+    for(int j = 0; j < k_size ; j+=4){
+        x1 = q_centroids[j];
+        y1 = q_centroids[j+1];
                 
-                tmp = (x2 - x1)*(x2 - x1)+ (y2 - y1)*(y2 - y1);
+        tmp = (x2 - x1)*(x2 - x1)+ (y2 - y1)*(y2 - y1);
 
-                if(tmp < min_dist){
-                    min_dist = tmp;
-                    cluster_atual = clust;
-                }
-                clust++;
+        if(tmp < min_dist){
+            min_dist = tmp;
+            cluster_atual = clust;
         }
-    
-        q_cluster_atribution[id] = cluster_atual;
+        clust++;
+    }
+    q_cluster_atribution[id] = cluster_atual;
 }
-
 
 //Inicializa os diferentes arrays
 void inicializa() {
-    
     int k=0;
 
     pontos = (float*) malloc(N*2*sizeof(float));
@@ -74,34 +63,24 @@ void inicializa() {
     centroids = (float*) malloc(K*4*sizeof(float));
 
     srand(10);
-
-    
-
    
-        for(int i = 0; i < N*2; i+=2) {
-            pontos[i] = (float) rand() / RAND_MAX;
-            pontos[i+1] = (float) rand() / RAND_MAX;
-        }
-    
-
-    
+    for(int i = 0; i < N*2; i+=2) {
+        pontos[i] = (float) rand() / RAND_MAX;
+        pontos[i+1] = (float) rand() / RAND_MAX;
+    }
     for(int i = 0; i < K*2*2; i+=2) {
         centroids[i+i] = pontos[i];     //Centroid ponto x
         centroids[i+i+1] = pontos[i+1]; //Centroid ponto y
         cluster_size[k] = 0;
         k++;
-
     }
-
     cudaMalloc ((void**) &d_pontos,  N*2*(sizeof(float)) );
     cudaMalloc ((void**) &d_centroids, K*4*sizeof(float));
     cudaMalloc ((void**) &d_cluster_atribution, N*sizeof(int));
     cudaMalloc ((void**) &d_cluster_size, K*sizeof(int));
 
-       
     cudaMemcpy(d_centroids, centroids, K*4*sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(d_pontos, pontos, N*2*sizeof(float), cudaMemcpyHostToDevice);
-
 
     cluster_distrib<<< NUM_THREADS_PER_BLOCK,NUM_BLOCKS >>> (d_cluster_size,d_centroids,d_pontos,d_cluster_atribution);
 
@@ -116,28 +95,17 @@ int calculate_centroid(){
     for(int i = 0; i < K*4; i+=4){
         centroids[i+2] = centroids[i];
         centroids[i+3] = centroids[i+1];
-
         centroids[i] = 0.0;
         centroids[i+1] = 0.0;
 
     }
-
-    // area de multithread  (2)
-    
     for(int i = 0; i < N; i++){
         int cluster = cluster_atribution[i];
         centroids[cluster*4] += pontos[i*2];
         centroids[cluster*4+1] += pontos[i*2+1];
-        
         cluster_size[cluster]++;
     }
-
-
-
     k=0;
-
-
-    
     
     //Divisão dos valores dos centroids pelo tamanho do seu respetivo cluster, para obter o novo centroid do cluster
     for(int i = 0; i < K*4; i+=4){
@@ -145,15 +113,12 @@ int calculate_centroid(){
             centroids[i] = centroids[i]/cluster_size[k];
             centroids[i+1] = centroids[i+1]/cluster_size[k];
         }
-
         if((centroids[i] != centroids[i+2]) || (centroids[i+1] !=centroids[i+3])) end = 0;
         if(end == 0 && iteracoes+1 <20){
             cluster_size[k] = 0;
         }
-        k++;
-        
+        k++;   
     }
-
     return end;
 }
 
@@ -161,14 +126,12 @@ int calculate_centroid(){
 void k_means(){
     int end = 0;
 
-
     //Ciclo que para quando os centroids não variam
     while(end == 0 && iteracoes <20){
         end = calculate_centroid();
         cudaMemcpy(d_centroids, centroids, K*4*sizeof(float), cudaMemcpyHostToDevice);
         cluster_distrib<<< NUM_THREADS_PER_BLOCK,NUM_BLOCKS >>> (d_cluster_size,d_centroids,d_pontos,d_cluster_atribution);
         cudaMemcpy(cluster_atribution,d_cluster_atribution,N*sizeof(int),cudaMemcpyDeviceToHost);
-
         iteracoes++;
     }
     printf("N = %d, K = %d\n",N,K);
@@ -179,9 +142,7 @@ void k_means(){
 }
 
 int main(int argc, char **argv){
-    
     inicializa();
     k_means();
-    
     return 0;
 }
